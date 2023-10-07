2d84c308f24af8ed91ae9ed7ae1aaa80db0e8d5a
raulgamero <raulgamero03@gmail.com>
2023-10-07 22:16:16 +0200

    check-errors-v3

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ CLUSTER = os.environ["CLUSTER"]
 
 def mongo_connection():
     print(USER, PASSWORD, CLUSTER)
-    uri = "mongodb+srv://{USER}:{PASSWORD}@{CLUSTER}.wocrudb.mongodb.net/?retryWrites=true&w=majority"
+    uri = "mongodb+srv://" + USER + ":" + PASSWORD + "@" + CLUSTER + ".wocrudb.mongodb.net/?retryWrites=true&w=majority"
     # Create a new client and connect to the server
     client = MongoClient(uri, server_api=ServerApi('1'))
     # Send a ping to confirm a successful connection
